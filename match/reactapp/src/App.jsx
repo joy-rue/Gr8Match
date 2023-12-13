5cde7999c8d4c3afb83f38242d395ac8b29eb16e
Arachnid76 <88135722+Arachnid76@users.noreply.github.com>
2023-12-13 10:56:33 +0000

    login works

--- match/reactapp/src/App.jsx
+++ match/reactapp/src/App.jsx
@@ -1,17 +1,27 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import Login from "./Login";
 import HomePage from "./HomePage";
-import ProjectPage from "./ProjectPage";
 import { AuthProvider } from './AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const handleFormSwitch = (formType) => {
+    // Logic to switch between login and registration forms
+    console.log("Switching to", formType);
+  };
+
   return (
     <AuthProvider>
-    <div className="app__body">
-      <HomePage />
-    </div>
+      <Router>
+        <Routes>
+          <Route path="//*" element={showLoginForm ? <Login onFormSwitch={handleFormSwitch} /> : <HomePage />} />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
-}
+};
 
 export default App;
--- match/reactapp/src/Login.jsx
+++ match/reactapp/src/Login.jsx
@@ -3,14 +3,17 @@ import axios from 'axios';
 import ashesicampus from "./components/icons/ashesicampus.jpg";
 import ashesilogo from "./components/icons/ashesilogo.png";
 import { AuthProvider, useAuth } from './AuthContext';
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage'; // Import HomePage
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
-  const { setToken } = useAuth();  // Use the useAuth hook to get setToken function
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent the default form submission
+    e.preventDefault();
 
     try {
       const response = await axios.post('http://127.0.0.1:8000/accounts/login/', {
@@ -18,116 +21,88 @@ const Login = (props) => {
         password: pass,
       });
 
-      // Handle the response (e.g., update state, show success message, etc.)
       const { token } = response.data;
       console.log('Token:', token);
-      setToken(token);// Set the token in the AuthContext
+      setToken(token);
+      navigate("/home");
+
     } catch (error) {
       console.error('Error:', error.message);
     }
   };
 
   return (
-  <AuthProvider>
-    <div
-      style={{
-        position: "relative",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <img src={ashesicampus} alt="Ashesi Campus" style={{ width: "100%" }} />
-      <div
-        className="auth-form-container"
-        style={{
-          position: "absolute",
-          padding: "20px",
-          width: "400px",
-          background: "rgba(255, 255, 255, 0.636)",
-          borderRadius: "10px",
-          textAlign: "center",
-        }}
-      >
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <img
-            src={ashesilogo}
-            alt="Ashesi Logo"
-            style={{
-              width: "50%",
-              margin: "20px",
-            }}
-          />
-          <h4 style={{ marginBottom: "30px", textAlign: "left" }}>Login</h4>
-        </div>
-        <form
-          className="login-form"
-          onSubmit={handleSubmit}
-          style={{
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div style={{
+            position: "relative",
             display: "flex",
-            flexDirection: "column",
-            alignItems: "left",
-          }}
-        >
-          <label htmlFor="email" style={{ textAlign: "left" }}>
-            Email
-          </label>
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            id="email"
-            name="email"
-            style={{
-              marginBottom: "10px",
-            }}
-          />
-          <label htmlFor="password" style={{ textAlign: "left" }}>
-            Password
-          </label>
-          <input
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-            type="password"
-            id="password"
-            name="password"
-            style={{
-              marginBottom: "10px",
-            }}
-          />
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}>
+            <img src={ashesicampus} alt="Ashesi Campus" style={{ width: "100%" }} />
+            <div className="auth-form-container" style={{
+              position: "absolute",
+              padding: "20px",
+              width: "400px",
+              background: "rgba(255, 255, 255, 0.636)",
+              borderRadius: "10px",
+              textAlign: "center",
+            }}>
+              <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <img src={ashesilogo} alt="Ashesi Logo" style={{
+                  width: "50%",
+                  margin: "20px",
+                }} />
+                <h4 style={{ marginBottom: "30px", textAlign: "left" }}>Login</h4>
+              </div>
+              <form className="login-form" onSubmit={handleSubmit} style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "left",
+              }}>
+                <label htmlFor="email" style={{ textAlign: "left" }}>
+                  Email
+                </label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" style={{
+                  marginBottom: "10px",
+                }} />
+                <label htmlFor="password" style={{ textAlign: "left" }}>
+                  Password
+                </label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" style={{
+                  marginBottom: "10px",
+                }} />
 
-<button
-        type="submit"
-        onClick={handleSubmit}
-        style={{
-          marginTop: "20px",
-          width: "30%",
-          color: "white",
-          background: "#AD3537",
-          borderRadius: "5px",
-          borderColor: "#AD3537",
-          marginLeft: "35%",
-        }}
-      >
-        Login
-      </button>
+                <button type="submit" onClick={handleSubmit} style={{
+                  marginTop: "20px",
+                  width: "30%",
+                  color: "white",
+                  background: "#AD3537",
+                  borderRadius: "5px",
+                  borderColor: "#AD3537",
+                  marginLeft: "35%",
+                }}>
+                  Login
+                </button>
 
-        </form>
-        <div
-          className="link-btn"
-          onClick={() => props.onFormSwitch("login")}
-          style={{
-            marginTop: "20px",
-            marginBottom: "20px",
-            textAlign: "center", // Centered the text
-          }}
-        >
-          Don't have an account? Register here.
-        </div>
-      </div>
-    </div>
-    </AuthProvider>
+              </form>
+              <div className="link-btn" onClick={() => props.onFormSwitch("login")} style={{
+                marginTop: "20px",
+                marginBottom: "20px",
+                textAlign: "center", // Centered the text
+              }}>
+                Don't have an account? Register here.
+              </div>
+            </div>
+          </div>
+        }
+      />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
   );
 };
 
