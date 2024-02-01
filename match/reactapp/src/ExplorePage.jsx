026155abb8e0064560fa7f61d6aa15035bf63154
joy-rue <ruejoysithole@gmail.com>
2024-02-01 00:17:57 +0000

    commit -m "conflict resolution"

--- match/reactapp/src/ExplorePage.jsx
+++ match/reactapp/src/ExplorePage.jsx
@@ -57,17 +57,6 @@ const ExplorePage = () => {
 
               if (responseProjectTeam.status === 200) {
                 const teamData = responseProjectTeam.data;
-<<<<<<< HEAD
-                console.log(teamData)
-                  ; setAllProjectsData((prevProjects) => ({
-                    ...prevProjects,
-                    [project.id]: {
-                      projectData: project,
-                      teamData: teamData,
-                    },
-                  }));
-                console.log(projects, "data here:")
-=======
                 console.log(teamData);
                 setAllProjectsData((prevProjects) => ({
                   ...prevProjects,
@@ -77,7 +66,6 @@ const ExplorePage = () => {
                   },
                 }));
                 console.log(projects, "data here:");
->>>>>>> fa9007de822d3fd50ac08af696e85c7cb86ab5a1
               } else {
                 console.error(
                   `Failed to fetch team members for project ${project.id}:`,
@@ -106,63 +94,6 @@ const ExplorePage = () => {
 
   // Access projects dictionary where each project has projectData and teamData
   console.log(projects);
-<<<<<<< HEAD
-
-
-  const handleTokenRefresh = async () => {
-    try {
-      // Make a request to your backend to refresh the access token using the refresh token
-      const refreshResponse = await axios.post(
-        "http://127.0.0.1:5173/api/account/refresh/",
-        {
-          refresh: Cookies.get("refresh"),
-        }
-      );
-
-      const newAccessToken = refreshResponse.data.access;
-
-      // Update the access token in cookies
-      Cookies.set("access", newAccessToken);
-
-      // Retry the original API request with the new access token
-      await fetchData();
-    } catch (error) {
-      console.error("Error refreshing token:", error);
-    }
-  };
-
-  var allProjects = [];
-
-  if (projects) {
-    Object.keys(projects).some(projectKey => {
-      const project = projects[projectKey];
-  
-      // Access the teamData for the current project
-      const teamData = project.teamData;
-  
-      var is_member = Object.keys(project.projectData.team_members).some(memberKey => {
-        const member = project.projectData.team_members[memberKey];
-        return member.id === id;
-      });
-      console.log(project.projectData.id);
-  
-      if (is_member) {
-        return true; // Skip to the next project
-      }
-      const projectallProjects = (
-
-        <ApplytoProject
-          id={project.projectData.id}
-          profile={ashesilogoblank}
-          title={project.projectData.title}
-          dueDate={project.projectData.start_date
-            ? moment(project.projectData.start_date).format("Do MMM YYYY") + "-" + moment(project.projectData.end_date).format("Do MMM YYYY")
-            : "-- -- --"}
-          workhours={project.projectData.estimated_project_hours ? project.projectData.estimated_project_hours : "0"
-          }
-          People={project.projectData.user ? project.projectData.user : ["assignment pending.."]}
-          description={project.projectData.description ? project.projectData.description : " "}
-=======
 
   const handleTokenRefresh = async () => {
     try {
@@ -234,20 +165,12 @@ const ExplorePage = () => {
               ? project.projectData.description
               : " "
           }
->>>>>>> fa9007de822d3fd50ac08af696e85c7cb86ab5a1
         />
       );
 
       allProjects.push(projectallProjects);
-<<<<<<< HEAD
-      ;
-
-    })
-  };
-=======
     });
   }
->>>>>>> fa9007de822d3fd50ac08af696e85c7cb86ab5a1
 
   return (
     <div>
